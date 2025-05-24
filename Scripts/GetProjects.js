961750c2638214f3c696bd502396d3d23b9ba22f
Aryan Bataan <97193906+AryanB11@users.noreply.github.com>
2025-05-24 16:07:29 -0700

    Update GetProjects.js

--- Scripts/GetProjects.js
+++ Scripts/GetProjects.js
@@ -1,24 +1,24 @@
 import { ConfigSingleton } from "./GetProfile.js";
+
 let configData;
 
 (async () => {
   try {
-    const configInstance = await ConfigSingleton.getInstance();
-    configData = configInstance.getConfig();
-    updateHTML(configData);
-  } catch (error) {
-    console.error('Error:', error);
-  }
-})();
-
+      const configInstance = await ConfigSingleton.getInstance();
+      configData = configInstance.getConfig();
+      updateHTML(configData);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  })();
+  
 function updateHTML(configData) {
   let projectTitle;
 
   document.querySelector("#pfThumbnail").src = "Content/".concat(configData.About.Thumbnail);
   document.querySelector("#pfLinkedIn").href = configData.Contact.LinkedIn;
   document.querySelector("#pfGitHub").href = configData.Contact.GitHub;
-
-  // ------- Project 1 -------
+  
   projectTitle = configData.Project1.Title;
   if (projectTitle) {
     document.querySelector("#pfProject1Img").src = "Content/".concat(configData.Project1.MainImage);
@@ -28,14 +28,13 @@ function updateHTML(configData) {
     if (!configData.Project1.GitHubRepo) {
       document.querySelector("#pfProject1Repo").style.display = "none";
     }
-    if (configData.Project1.DetailImages.length < 1) {
+    if (configData.Project1.DetailImages.length<1) {
       document.querySelector("#openProject1").style.display = "none";
     }
   } else {
     document.querySelector("#project1").style.display = "none";
   }
-
-  // ------- Project 2 -------
+  
   projectTitle = configData.Project2.Title;
   if (projectTitle) {
     document.querySelector("#pfProject2Img").src = "Content/".concat(configData.Project2.MainImage);
@@ -45,14 +44,13 @@ function updateHTML(configData) {
     if (!configData.Project2.GitHubRepo) {
       document.querySelector("#pfProject2Repo").style.display = "none";
     }
-    if (configData.Project2.DetailImages.length < 1) {
+    if (configData.Project2.DetailImages.length<1) {
       document.querySelector("#openProject2").style.display = "none";
     }
   } else {
     document.querySelector("#project2").style.display = "none";
   }
 
-  // ------- Project 3 -------
   projectTitle = configData.Project3.Title;
   if (projectTitle) {
     document.querySelector("#pfProject3Img").src = "Content/".concat(configData.Project3.MainImage);
@@ -62,14 +60,13 @@ function updateHTML(configData) {
     if (!configData.Project3.GitHubRepo) {
       document.querySelector("#pfProject3Repo").style.display = "none";
     }
-    if (configData.Project3.DetailImages.length < 1) {
+    if (configData.Project3.DetailImages.length<1) {
       document.querySelector("#openProject3").style.display = "none";
     }
   } else {
     document.querySelector("#project3").style.display = "none";
   }
-
-  // ------- Project 4 -------
+  
   projectTitle = configData.Project4.Title;
   if (projectTitle) {
     document.querySelector("#pfProject4Img").src = "Content/".concat(configData.Project4.MainImage);
@@ -79,7 +76,7 @@ function updateHTML(configData) {
     if (!configData.Project4.GitHubRepo) {
       document.querySelector("#pfProject4Repo").style.display = "none";
     }
-    if (configData.Project4.DetailImages.length < 1) {
+    if (configData.Project4.DetailImages.length<1) {
       document.querySelector("#openProject4").style.display = "none";
     }
   } else {
@@ -87,13 +84,14 @@ function updateHTML(configData) {
   }
 }
 
-// ---------- Shared Modal Image Functions ----------
-
 function addImages(imgs) {
+  // First remove any existing images
   let il = document.querySelector("#imgList");
-  while (il.firstChild) il.removeChild(il.firstChild);
+  while(il.firstChild) il.removeChild(il.firstChild);
+
+  // Then add them in from the profile
   for (let i = 0; i < imgs.length; i++) {
-    addListItem(imgs[i]);
+    addListItem(imgs[i])
   }
 }
 
@@ -104,60 +102,51 @@ function addListItem(newImg) {
   il.appendChild(newElem);
 }
 
+/* Handle the Carousel */
 let slideIdx = 1;
-
-function showImages(n) {
-  const slides = document.querySelectorAll("#imgList>img");
-  if (n > slides.length) { slideIdx = 1; }
-  if (n < 1) { slideIdx = slides.length; }
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slideIdx - 1].style.display = "block";
-}
 
 document.querySelector("#next").onclick = () => {
   showImages(++slideIdx);
-};
+}
 
 document.querySelector("#prev").onclick = () => {
   showImages(--slideIdx);
-};
+}
 
-// ---------- Project Modal Triggers ----------
+function showImages(n) {
+  const slides = document.querySelectorAll("#imgList>img");
+  if (n > slides.length) { slideIdx = 1 }
+  if (n < 1) { slideIdx = slides.length }
+  for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIdx - 1].style.display = "block";
+}
 
 document.querySelector("#openProject1").onclick = () => {
   addImages(configData.Project1.DetailImages);
   document.querySelector("#modalPage").style.display = "block";
   showImages(slideIdx);
-};
+}
 
 document.querySelector("#openProject2").onclick = () => {
   addImages(configData.Project2.DetailImages);
   document.querySelector("#modalPage").style.display = "block";
   showImages(slideIdx);
-};
+}
 
 document.querySelector("#openProject3").onclick = () => {
   addImages(configData.Project3.DetailImages);
   document.querySelector("#modalPage").style.display = "block";
   showImages(slideIdx);
-};
-
-document.querySelector("#openProject4").onclick = () => {
-  addImages(configData.Project4.DetailImages);
-  document.querySelector("#modalPage").style.display = "block";
-  showImages(slideIdx);
-};
-
-// ---------- Modal Dismissal Logic ----------
+}
 
 document.querySelector("#XOut").onclick = () => {
   document.querySelector("#modalPage").style.display = "none";
-};
+}
 
 window.onclick = (event) => {
   if (event.target == document.querySelector("#modalPage")) {
-    document.querySelector("#modalPage").style.display = "none";
+      document.querySelector("#modalPage").style.display = "none";
   }
-};
+}
